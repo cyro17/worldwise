d3884c0c0a5ecaa5034a2af9705618cc7db0c2b2
Cyro <coolronitchs@gmail.com>
2024-03-03 02:36:19 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav";
+
+function HomePage() {
+  return (
+    <div>
+      <PageNav />
+      <h1>World Wise </h1>
+    </div>
+  );
+}
+
+export default HomePage;
