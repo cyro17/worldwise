d0d36e41ffdf881a1321b3dd0130e5ce1dbd6124
Cyro <coolronitchs@gmail.com>
2024-03-04 01:02:41 +0530

    components and pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 /* eslint-disable no-unused-vars */
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
-import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
+import HomePage from "./pages/HomePage";
+import Pricing from "./pages/Pricing";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import Logo from "./Logo";
+import AppNav from "./AppNav";
+import { Outlet } from "react-router-dom";
+import styles from "./Sidebar.module.css";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <Outlet />
+
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default Sidebar;
